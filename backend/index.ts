a7e6a975de78c9a65efe5888c11de4de8274852c
Belenov Dmitriy <dimabelenov@gmail.com>
2026-05-27 15:28:43 +0300

    refactor: initialize Express application and move server entry point to src/app.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import app from './src/app';
+import { connectDB } from './src/config/database';
+const PORT = process.env.PORT || 3080;
+
+connectDB()
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
+	})
+	.catch((err) => {
+		console.error(`Server connection failed: ${err}`);
+	});
