dcae89300a6b29d92de11fd742df8f6f0771af54
Belenov Dmitriy <dimabelenov@gmail.com>
2026-05-27 16:36:01 +0300

    feat: initialize API route structures and add build script to package.json

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,15 @@
 import express from 'express';
+import authRoutes from './routes/auth.routes';
+import messageRoutes from './routes/message.routes';
+import chatRoutes from './routes/chat.routes';
+import usersRoutes from './routes/user.routes';
 
 const app = express();
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/chats', chatRoutes);
+app.use('/api/users', usersRoutes);
 
 export default app;
